01c4cbd7c70f008ec2d60f04ce38453cefe596b9
newdive <2938617010@qq.com>
2020-06-06 02:07:59 +0800

    [feat] 同步功能更新

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ import os
 import zipfile
 import optparse
 import tools
+import time
 
 if sys.version_info[0] != 2:
     print(u"当前的Python版本: {}。 该程序只能在Python2.7下运行。".format(sys.version.split(' ')[0]))
@@ -14,24 +15,35 @@ if __name__ == '__main__':
     parser = optparse.OptionParser()
     parser.add_option('-o','--output',
         action='store',dest='output', 
-        help = u'提取文件存放的目录(默认存放到apk所在的目录下)',default=False)
-    parser.add_option('-v','--viewKey',
-        action='store_true',dest='viewKey',
-        help = u'查看rc4的key',default=False)
+        help = u'提取文件存放的目录(默认存放到apk所在的目录下)',default='')
+    parser.add_option('-v','--viewInfo',
+        action='store_true',dest='viewInfo',
+        help = u'查看rc4的key等信息',default=False)
     
     options,args = parser.parse_args()
     #print(options)
     #print(args)
     if not args:
-        print(u'没有指定apk文件')
+        print(u'没有指定apk文件/文件夹')
         sys.exit()
-    if not zipfile.is_zipfile(args[0]):
+    if args[0] and not os.path.exists(args[0]):
+        print(u'没有指定apk文件/文件夹')
+        sys.exit()
+    if args[0] and not os.path.isdir(args[0]) and not zipfile.is_zipfile(args[0]):
         print(u'{} 不是apk文件'.format(args[0]))
         sys.exit()
-    if options.viewKey:
-        rc4Key = tools.extractRC4KeyFromApk(args[0])
-        if rc4Key:
-            print(rc4Key)
+
+    if options.viewInfo:
+        apkInfos = tools.extractAPICloudApkInfos(args[0],True)
+        for apk,apkInfo in apkInfos.items():
+            print(apk)
+            print(u'\tpackage      : {}\n\tuz_version   : {}\n\tencrypted    : {}\n\trc4Key       : {}\n'.format(apkInfo['package'], \
+                                                                        apkInfo['uz_version'], \
+                                                                        apkInfo['encrypted'], \
+                                                                        apkInfo['resKey']))
+        print(u'共找到 {} 个 apicloud apk'.format(len(apkInfos)))
     else:
         outputFolder = options.output
-        extractMap = tools.decryptAllResourcesInApk(args[0],outputFolder,printLog=True)
+        startTime = time.time()
+        extractMap = tools.decryptAndExtractAPICloudApkResources(args[0],outputFolder,printLog=True)
+        print(u'耗时 : {} 秒'.format(time.time()-startTime))
