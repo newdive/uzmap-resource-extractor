2175da6cd07bb59d16f1cb5d392e5a7880d3d412
newdive <2938617010@qq.com>
2020-09-08 19:06:05 +0800

    fix issue #5 修改在小于256个字节情况下的熵值计算逻辑

--- entropy.py
+++ entropy.py
@@ -7,7 +7,7 @@ for 256 values in a byte, the maximum entropy is log2(256)/256*256 = 8
 this method is a bit slow though
 '''
 def shannonEntropy(dataBytes):
-    entropy = 0
+    entropy,byteValNum = 0,1
     if dataBytes:
         dataFreq = collections.Counter(dataBytes)
         length = len(dataBytes)
@@ -15,8 +15,9 @@ def shannonEntropy(dataBytes):
         for k,freq in dataFreq.items():
             p_x = float(freq)/length
             entropy -= p_x * math.log(p_x, 2)
+        byteValNum = min(length,256)
 
-    return entropy/8
+    return entropy/max(1,math.log(byteValNum,2))
 
 '''
 this method is fast, in the cost of some accuracy(not as good as shanno formula)
